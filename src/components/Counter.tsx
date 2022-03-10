0f97b15432c9e7476fee8c3806d1be7189cea903
Vadym Barabanov <introvert.png@gmail.com>
2022-03-10 21:26:40 +0200

    Add button component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,20 +2,22 @@ import { observer } from 'mobx-react-lite';
 import React, { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { counter } from '../mobx/Counter';
+import { Variant } from '../types/styles';
+import { Button } from './Button';
+import { useTheme } from './ThemeProvider';
 
 const Counter = () => {
   const [u, setu] = useState(0);
+  const { toggleTheme } = useTheme();
+
   const { count, increase, decrease } = counter;
 
   return (
     <View>
       <Text>I love Vadim Barabanov {count}</Text>
-      <TouchableOpacity onPress={increase}>
-        <Text>+</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={decrease}>
-        <Text>-</Text>
-      </TouchableOpacity>
+      <Button text="+" onPress={increase} />
+      <Button variant={Variant.SECONDARY} text="-" onPress={decrease} />
+      <Button text="Toggle Theme" onPress={toggleTheme} />
 
       <TouchableOpacity onPress={() => setu(Math.random())}>
         <Text>{u}</Text>
