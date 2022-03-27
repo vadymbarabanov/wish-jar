8fed8ac488f0d8ee5a55e3079abe61cc576f96e2
Polina <polinakaryzhska@gmail.com>
2022-03-27 16:27:19 +0300

    Fix styles

--- src/components/Buttons/SettingsButton/styles.ts
+++ src/components/Buttons/SettingsButton/styles.ts
@@ -6,14 +6,17 @@ export const createStyles: CreateStyles = (theme) => {
     button: {
       flexDirection: 'row',
       alignItems: 'center',
+
+      marginVertical: 10,
       paddingVertical: 15,
       paddingHorizontal: 10,
-      marginVertical: 10,
+
       borderBottomWidth: 1,
       borderBottomColor: theme.primary,
     },
     text: {
       marginHorizontal: 20,
+
       fontSize: 20,
       color: theme.primary,
     },
